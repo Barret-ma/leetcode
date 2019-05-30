1b9e9b912818479548cf0bfb9aae8617bddb5e38
Barret-ma <chaoiseneu@gmail.com>
2019-05-30 20:23:48 +0800

    add 44 problem

--- 44. Wildcard Matching.py
+++ 44. Wildcard Matching.py
@@ -1,8 +1,47 @@
-#
-# @lc app=leetcode id=44 lang=python
-#
-# [44] Wildcard Matching
-#
+# Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+# '?' Matches any single character.
+# '*' Matches any sequence of characters (including the empty sequence).
+# The matching should cover the entire input string (not partial).
+
+# Note:
+
+# s could be empty and contains only lowercase letters a-z.
+# p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+# Example 1:
+
+# Input:
+# s = "aa"
+# p = "a"
+# Output: false
+# Explanation: "a" does not match the entire string "aa".
+# Example 2:
+
+# Input:
+# s = "aa"
+# p = "*"
+# Output: true
+# Explanation: '*' matches any sequence.
+# Example 3:
+
+# Input:
+# s = "cb"
+# p = "?a"
+# Output: false
+# Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+# Example 4:
+
+# Input:
+# s = "adceb"
+# p = "*a*b"
+# Output: true
+# Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+# Example 5:
+
+# Input:
+# s = "acdcb"
+# p = "a*c?b"
+# Output: false
 class Solution(object):
     def isMatch(self, s, p):
         """
@@ -15,21 +54,21 @@ class Solution(object):
         iStar = -1
         jStar = -1
         while i < len(s):
-            if s[i] == p[j] or p[j] == '?':
+            if j < len(p) and (s[i] == p[j] or p[j] == '?'):
                 i += 1
                 j += 1
-            elif p[j] == '*':
+            elif j < len(p) and p[j] == '*':
                 iStar = i
                 jStar = j
                 j += 1
             elif iStar >= 0:
                 iStar += 1
                 i = iStar
-                j = iStar + 1
+                j = jStar + 1
             else:
                 return False
         while j < len(p) and p[j] == '*':
             j += 1
         return j == len(p)
 s = Solution()
-print(s.isMatch('adceb', '*a*b'))
+print(s.isMatch('aa', 'a'))
