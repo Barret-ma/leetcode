912431f6fa724e935fa0ec56bd653369b537685a
Barret-ma <chaoiseneu@gmail.com>
2019-07-07 23:01:46 +0800

    add 987 solution

--- 987. Vertical Order Traversal of a Binary Tree.py
+++ 987. Vertical Order Traversal of a Binary Tree.py
@@ -1,11 +1,55 @@
+# Given a binary tree, return the vertical order traversal of its nodes values.
+
+# For each node at position (X, Y), its left and right children respectively will be at positions (X-1, Y-1) and (X+1, Y-1).
+
+# Running a vertical line from X = -infinity to X = +infinity, whenever the vertical line touches some nodes, we report the values 
+# of the nodes in order from top to bottom (decreasing Y coordinates).
+
+# If two nodes have the same position, then the value of the node that is reported first is the value that is smaller.
+
+# Return an list of non-empty reports in order of X coordinate.  Every report will have a list of values of nodes.
+
+ 
+
+# Example 1:
+
+
+
+# Input: [3,9,20,null,null,15,7]
+# Output: [[9],[3,15],[20],[7]]
+# Explanation: 
+# Without loss of generality, we can assume the root node is at position (0, 0):
+# Then, the node with value 9 occurs at position (-1, -1);
+# The nodes with values 3 and 15 occur at positions (0, 0) and (0, -2);
+# The node with value 20 occurs at position (1, -1);
+# The node with value 7 occurs at position (2, -2).
+# Example 2:
+
+
+
+# Input: [1,2,3,4,5,6,7]
+# Output: [[4],[2],[1,5,6],[3],[7]]
+# Explanation: 
+# The node with value 5 and the node with value 6 have the same position according to the given scheme.
+# However, in the report "[1,5,6]", the node value of 5 comes first since 5 is smaller than 6.
+ 
+
+# Note:
+
+# The tree will have between 1 and 1000 nodes.
+# Each node's value will be between 0 and 1000.
+
+
+
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
+    min = 0
     def verticalTraversal(self, root):
         """
         :type root: TreeNode
@@ -13,12 +57,42 @@ class Solution(object):
         """
         if not root:
             return []
-        result = []
+        results = dict()
+        self.travelTree(root, results, 0)
+        # print(len(results))
+        # print(results)
+        length = len(results)
+        orderList = [0] * length
+        maxLeft = abs(self.min)
+        for key, value in results.items():
+            orderList[maxLeft + key] = value
+        return orderList
+        
 
-        def travel(root, height):
-            if not root.left and not root.right:
-                pass
-            if root.left:
-                travel(root.left, height + 1)
-            if root.right:
-                travel(root.right, height - 1)
+    def travelTree(self, root, results, x):
+        if not root:
+            return
+        self.min = min(x, self.min)
+        if x not in results:
+            results[x] = [root.val]
+        else:
+            results[x].append(root.val)
+        self.travelTree(root.left, results, x - 1)
+        self.travelTree(root.right, results, x + 1)
+
+
+root = None
+root2 = TreeNode(2)
+root3 = TreeNode(3)
+root4 = TreeNode(4)
+root5 = TreeNode(5)
+root6 = TreeNode(6)
+root7 = TreeNode(7)
+# root.left = root2
+# root.right = root3
+# root2.left = root4
+# root2.right = root5
+# root3.left = root6
+# root3.right = root7
+s = Solution()
+print(s.verticalTraversal(root))
