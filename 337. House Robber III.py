f3f873ff9eb7ba4f903eb1ce90f940364e539eeb
Barret-ma <chaoiseneu@gmail.com>
2019-08-02 18:12:25 +0800

    add 337 solution

--- 337. House Robber III.py
+++ 337. House Robber III.py
@@ -40,6 +40,11 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        results = []
-        results.append[root.val]
-        results.append()
+        def rob(root):
+            if not root: return 0, 0, 0
+            l, ll, lr = rob(root.left)
+            r, rl, rr = rob(root.right)
+            return max(root.val + ll + lr + rl + rr, l + r), l, r
+    
+        return rob(root)[0]
+        
