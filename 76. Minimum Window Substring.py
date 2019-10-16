644275b01332fef22eda88d3e9fce90c964f9e8e
Barret-ma <chaoiseneu@gmail.com>
2019-10-16 19:32:15 +0800

    add 76 solution

--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -8,7 +8,7 @@
 
 # If there is no such window in S that covers all characters in T, return the empty string "".
 # If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
-
+from collections import defaultdict, Counter
 class Solution(object):
     def minWindow(self, s, t):
         """
@@ -16,12 +16,41 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
+        if not s or not t or len(s) < len(t):
+            return ''
         left = 0
         right = 0
-        hashMap = dict()
+        minCnt = float('inf')
+        # hashMap = defaultdict(lambda: 0)
+        result = ''
+        hashMap = Counter(t)
+        count = 0
         while right < len(s):
-            if s[right] in hashMap:
-                pass
-            hashMap[s[right]] = 1
+            hashMap[s[right]] -= 1
+
+            if (hashMap[s[right]] >= 0):
+                count += 1
+
+            while left < right and hashMap[s[left]] < 0:
+                hashMap[s[left]] += 1
+                left += 1
+                
+            if count == len(t) and minCnt > (right - left + 1):
+                minCnt = right - left + 1
+                result = s[left:right + 1]
+            right += 1
+        print(result)
+        return result
             
+s = Solution()
+s.minWindow('ADOBECODEBANC', 'ABC')
+# s.minWindow('aaflslflsldkalskaaa', 'aaa')
+# s.minWindow('aa', 'aa')
+            # while targetStrMap[s[right]] > 1:
+            #     if s[left] in targetStrMap and targetStrMap[s[left]] > 1:
+            #         left ++
+
+
+            
+
             
