664554caa62beda6ecd87b75d2560268fc12e533
Barret-ma <chaoiseneu@gmail.com>
2019-10-18 19:01:28 +0800

    add 322 problem

---
+++ 322. Coin Change.py
@@ -0,0 +1,27 @@
+# You are given coins of different denominations 
+# and a total amount of money amount. Write a function 
+# to compute the fewest number of coins that you need 
+# to make up that amount. If that amount of money cannot 
+# be made up by any combination of the coins, return -1.
+
+# Example 1:
+
+# Input: coins = [1, 2, 5], amount = 11
+# Output: 3 
+# Explanation: 11 = 5 + 5 + 1
+# Example 2:
+
+# Input: coins = [2], amount = 3
+# Output: -1
+# Note:
+# You may assume that you have an infinite number of each kind of coin.
+
+# dp[i] 为 i面值需要硬币的个数
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
