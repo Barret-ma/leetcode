d88e1ab4965933eab504ff5b4086c1d6559a09da
Barret-ma <chaoiseneu@gmail.com>
2019-10-29 20:30:42 +0800

    add 97 solution

--- 97. Interleaving String.py
+++ 97. Interleaving String.py
@@ -20,37 +20,24 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
-        # ls1 = len(s1)
-        # ls2 = len(s2)
-        # ls3 = len(s3)
-        # if ls1 + ls2 != ls3:
-        #     return False
-        # dp = [[False] * (ls2 + 1) for _ in range(ls1 + 1)]
-        # dp[0][0] = True
-        # for i in range(1, ls1):
-        #     dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
-        # for j in range(1, ls2):
-        #     dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
-        # for i in range(1, ls1):
-        #     for j in range(1, ls2):
-        #         dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i - 1 + j]) or (dp[i][j - 1] and s2[j - 1] == s3[j - 1 + i])
-        # return dp[ls1][ls2]
 
-        if len(s1)+len(s2)!=len(s3):
+        l1 = len(s1)
+        l2 = len(s2)
+        if l1 + l2 != len(s3):
             return False
-        DP=[[False]*(len(s2)+1) for i in range(len(s1)+1)]
-        for i in range(len(s1)+1):
-            for j in range(len(s2)+1):
-                if i==0 and j==0:
-                    DP[i][j]=True
-                elif i>0 and DP[i-1][j] and s3[i+j-1]==s1[i-1]:
-                    DP[i][j]=True
-                elif j>0 and DP[i][j-1] and s3[i+j-1]==s2[j-1]:
-                    DP[i][j]=True
-                else: #由于DP被初始化为全False，因此，该else语句可以省去
-                    DP[i][j]=False
+        dp = [[False for _ in range(l2 + 1)] for _ in range(l1 + 1)]
+        dp[0][0] = True
+        for i in range(1, l1 + 1):
+            dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
+        for j in range(1, l2 + 1):
+            dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
 
-        return DP[len(s1)][len(s2)]
+        for i in range(1, l1 + 1):
+            for j in range(1, l2 + 1):
+                dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i - 1 + j]) or (dp[i][j - 1] and s2[j - 1] == s3[j - 1 + i])
+        return dp[l1][l2]
+
+
 
 
 s = Solution()
