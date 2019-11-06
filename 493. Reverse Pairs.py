cb4f327195900870d97bb65f6483c63f8e27ad6f
Barret-ma <chaoiseneu@gmail.com>
2019-11-06 18:07:44 +0800

    add 493 solution

--- 493. Reverse Pairs.py
+++ 493. Reverse Pairs.py
@@ -14,6 +14,44 @@
 # Note:
 # The length of the given array will not exceed 50,000.
 # All the numbers in the input array are in the range of 32-bit integer.
+# class BIT:
+#     def __init__(self, n):
+#         self.n = n + 1
+#         self.sums = [0] * self.n
+    
+#     def update(self, i, delta):
+#         while i < self.n:
+#             self.sums[i] += delta
+#             i += i & (-i)
+    
+#     def query(self, i):
+#         res = 0
+#         while i > 0:
+#             res += self.sums[i]
+#             i -= i & (-i)
+#         return res
+# class Solution(object):
+#     def reversePairs(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         # BIT O(nlogn)
+#         new_nums = nums + [x * 2 for x in nums]
+#         sorted_set = sorted(list(set(new_nums)))
+#         tree = BIT(len(sorted_set))
+#         res = 0
+#         ranks = {}
+#         for i, n in enumerate(sorted_set):
+#             ranks[n] = i + 1
+
+#         for n in nums[::-1]:
+#             res += tree.query(ranks[n] - 1)
+#             tree.update(ranks[n * 2], 1)
+
+#         return res
+
+import copy
 class Solution(object):
     def reversePairs(self, nums):
         """
@@ -22,7 +60,7 @@ class Solution(object):
         """
         res = 0
         n = len(nums)
-        v = nums
+        v = copy.deepcopy(nums)
         bit = [0] * (n + 1)
         v.sort()
         m = dict()
@@ -43,8 +81,8 @@ class Solution(object):
 
         for i in range(n):
             m[v[i]] = i + 1
-        for i in range(n - 1, 0, -1):
-            res += self.getSum(lower_bound(nums, nums[i] / 2.0) - v[0], bit)
+        for i in range(n - 1, -1, -1):
+            res += self.getSum(lower_bound(v, nums[i] / 2.0) - 0, bit)
             self.update(m[nums[i]], bit)
         return res
 
@@ -58,7 +96,5 @@ class Solution(object):
         while i < len(bit):
             bit[i] += 1
             i += (i & -i)
-
-
 s = Solution()
-print(s.reversePairs([1,3,2,3,1]))
+print(s.reversePairs([5,4,3,2,1]))
