39e6afb6e814dac2e2cea3eaf3e823c0e0737733
Barret-ma <chaoiseneu@gmail.com>
2020-01-17 13:33:41 +0800

    add 438 solution

--- 438. Find All Anagrams in a String.py
+++ 438. Find All Anagrams in a String.py
@@ -28,40 +28,66 @@
 # The substring with start index = 1 is "ba", which is an anagram of "ab".
 # The substring with start index = 2 is "ab", which is an anagram of "ab".
 
-from collections import Counter
-
+# from collections import Counter
+from collections import defaultdict
 class Solution(object):
     def findAnagrams(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        if not s:
-            return []
-        pLen = len(p)
-        left = 0
-        right = 0
-        results = []
-        hashMap = Counter(p)
-        count = len(p)
+#         """
+#         :type s: str
+#         :type p: str
+#         :rtype: List[int]
+#         """
+#         if not s:
+#             return []
+#         pLen = len(p)
+#         left = 0
+#         right = 0
+#         results = []
+#         hashMap = Counter(p)
+#         count = len(p)
 
-        while right < len(s):
-            hashMap[s[right]] -= 1
-            if hashMap[s[right]] >= 0:
-                count -= 1
-            while (right - left + 1 > pLen or hashMap[s[left]] < 0) and left < right:
-                hashMap[s[left]] += 1
-                if s[left] in p and hashMap[s[left]] > 0:
-                    count += 1
+#         while right < len(s):
+#             hashMap[s[right]] -= 1
+#             if hashMap[s[right]] >= 0:
+#                 count -= 1
+#             while (right - left + 1 > pLen or hashMap[s[left]] < 0) and left < right:
+#                 hashMap[s[left]] += 1
+#                 if s[left] in p and hashMap[s[left]] > 0:
+#                     count += 1
+#                 left += 1
+#             if right - left + 1 == pLen and count == 0:
+#                 results.append(left)
+#             right += 1
+#         print(results)
+#         return results
+
+        if not s or not p:
+            return []
+        sArr = list(s)
+        pArr = list(p)
+        hashMap = defaultdict(lambda: 0)
+        for letter in pArr:
+            hashMap[letter] += 1
+            
+        left = 0
+        count = 0
+        size = len(pArr)
+        ans = []
+        for right in range(len(sArr)):
+            hashMap[sArr[right]] -= 1
+            if hashMap[sArr[right]] >= 0:
+                count += 1
+            if right - left + 1 >  size:
+                hashMap[sArr[left]] += 1
+                if (hashMap[sArr[left]] > 0):
+                    count -= 1
                 left += 1
-            if right - left + 1 == pLen and count == 0:
-                results.append(left)
-            right += 1
-        print(results)
-        return results
+            if count == size:
+                ans.append(left)
+                
+        return ans
 
 s = Solution()
 # s.findAnagrams('cbaebacbacd', 'abc')
 # s.findAnagrams('baxxxaa', 'aa')
-s.findAnagrams('aa', 'bb')
+print(s.findAnagrams('abab', 'ab'))
