c484787916e6e15399ba9e226e7f91fbba1ee776
Barret-ma <chaoiseneu@gmail.com>
2020-01-20 15:45:01 +0800

    add 32 solution

--- 32. Longest Valid Parentheses.py
+++ 32. Longest Valid Parentheses.py
@@ -18,20 +18,37 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        stack = []
+        # stack = []
+        # res = 0
+        # start = 0
+        # n = len(s)
+        # for i in range(n):
+        #     if s[i] == '(':
+        #         stack.append(i)
+        #     elif s[i] == ')':
+        #         if len(stack) == 0:
+        #             start = i + 1
+        #         else:
+        #             stack.pop()
+        #             if len(stack) == 0:
+        #                 res = max(res, i - start + 1)
+        #             else:
+        #                 res = max(res, i - stack[-1])
+        # return res
         res = 0
-        start = 0
         n = len(s)
-        for i in range(n):
-            if s[i] == '(':
-                stack.append(i)
-            elif s[i] == ')':
-                if len(stack) == 0:
-                    start = i + 1
-                else:
-                    stack.pop()
-                    if len(stack) == 0:
-                        res = max(res, i - start + 1)
-                    else:
-                        res = max(res, i - stack[-1])
+        dp = [0 for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            j = i - 2 - dp[i - 1]
+            if (s[i - 1] == '(' or j < 0 or s[j] == ')'):
+                dp[i] = 0
+            else:
+                dp[i] = dp[i - 1] + 2 + dp[j]
+                res = max(res, dp[i])
+
+
         return res
+
+s = Solution()
+s.longestValidParentheses('()(())')
